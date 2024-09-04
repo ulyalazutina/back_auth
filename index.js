75ba8b7f2b9dc6b30b49a776a6b5d0c0a80eb9e7
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-09-05 02:48:15 +0300

    cors

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const router = jsonServer.router('db.json')
 
 app.db = router.db
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(auth)
 app.use(router)
 app.use(cors())
