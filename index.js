56a5e85df9c726cb6f43d2342933236a478d75cf
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-09-05 02:49:21 +0300

    app

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = jsonServer.router('db.json')
 
 app.db = router.db
 
-server.use((req, res, next) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); 
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
