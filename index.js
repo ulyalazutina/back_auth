fcf1c56d17da098710a2594033f40c7e28478e93
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-09-05 02:30:19 +0300

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
+const cors = require("cors");
 
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -8,6 +9,7 @@ app.db = router.db
 
 app.use(auth)
 app.use(router)
+app.use(cors())
 app.listen(8080, () => {
-    console.log("JSON SERVER start: http://localhost:8080");
+    console.log("JSON SERVER work");
 })
